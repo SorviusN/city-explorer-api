38b278eb83eb10f5d84a5e9336042af0cf3a11ac
Jona <jbrown6@berklee.edu>
2021-06-18 23:52:27 -0700

    Unfinished cache

--- routeHandlers/getWeather.js
+++ routeHandlers/getWeather.js
@@ -1,23 +1,48 @@
+'use strict';
+
 const axios = require('axios');
+
+let cache = require('../cache.js');
 
 const weatherKey = process.env.WEATHER_API_KEY;
 
-class Forecast {
-  constructor(data) {
-    this.date = data.valid_date;
-    this.description =`low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description}`;
-    this.key = data.valid_date;
+module.exports = getWeather;
+
+function getWeather(lat, lon){
+  const key = 'weather-' + lat + lon;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherKey}`;
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit!');
+  }
+  else {
+    console.log('Cache miss!');
+    cache[key] = {};
+    cache[key.timestamp] = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseWeather(response.body));
+  }
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  }
+  catch (e) {
+    return Promise.reject(e);
   }
 }
 
+class Weather {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description =`low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
+    this.key = day.valid_date;
+  }
+}
 
-let getWeather = async (req, res) => {
-  let lon = req.query.lon;
-  let lat = req.query.lat;
-
-  let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherKey}`);
-
-  res.send(weatherData.data.data.map(forecastDay => new Forecast(forecastDay)));
-};
-
-module.exports = getWeather;
+//constructing a class for the Weather object that we will use.
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-// ---- initial config ----
+'use strict';
+
+require ('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-require('dotenv').config();
-
-//referencing our PORT from .env file
-const PORT = process.env.PORT;
-
-// ---- middleware (used to let client and server talk to eachother) ---
-// telling our express app that we are using cors (lets other servers talk to other servers)
 const cors = require('cors');
-app.use(cors()); 
+app.use(cors());
+
+
 //File routing to each routeHandler
 const root = require('./routeHandlers/root');
 const getWeather = require('./routeHandlers/getWeather');
@@ -19,10 +17,19 @@ const getMovies = require('./routeHandlers/getMovies');
 //referencing our root file to perform all of the code actions.
 app.get('/', root);
 
-//Getting the weather data.
-app.get('/weather', getWeather);
+function weatherHandler(req, res) {
+  const { lat, lon } = req.query;
+  weather(lat, lon)
+    .then(summaries => res.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      res.status(200).send('Sorry. Something went wrong!');
+    });
+}
 
-//getting the movie data. References 
+app.get('/weather', weatherHandler);
+
+//Getting the movie data. References
 app.get('/movie', getMovies);
 
 //any server that isn't specified above defaults to below.
@@ -31,13 +38,19 @@ app.get('/*',(res) => {
 });
 
 // config part 2
-app.listen(PORT, () => {
-  console.log(console.log(`listening on port ${PORT}`));
-});
+app.listen(process.env.PORT, () => {console.log(console.log(`Server up on ${process.env.PORT}`)); });
 
+// ---- initial config ----
+//const express = require('express');
+//const app = express();
 
+//require('dotenv').config();
 
+//referencing our PORT from .env file
+//const PORT = process.env.PORT;
 
-
-
-
+const PORT = process.env.PORT;
+// ---- middleware (used to let client and server talk to eachother) ---
+// telling our express app that we are using cors (lets other servers talk to other servers)
+//const cors = require('cors');
+//app.use(cors());
