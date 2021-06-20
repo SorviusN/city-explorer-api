edaffebd087cd0156d60cedb86faf35091f7f978
Jona <jbrown6@berklee.edu>
2021-06-20 00:52:43 -0700

    Promise is stuck on pending, data works up until last function called

--- routeHandlers/weather.js
+++ routeHandlers/weather.js
@@ -4,38 +4,41 @@
 //letting the file send requests to API
 const axios = require('axios');
 
+class Weather {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description =`low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
+  }
+}
+
 //setting our directory for the cache folder, where all of the data will be stored.
 let cache = require('../cache.js');
 
 //the key for weather API.
 const weatherKey = process.env.WEATHER_API_KEY;
 
-//exporting the getWeather folder for use in other areas.
-module.exports = getWeather;
-
 //starts by setting a key for the cache as well as creating an axios request
-function getWeather(lat, lon){
-  const key = 'weather-' + request.query.lat + request.query.lon;
+let getWeather = async(lat, lon) => {
+  const key = 'weather-' + lat + lon;
   const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherKey}`;
 
   if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
     console.log('Cache hit!', cache[key]);
   }
   else {
-    console.log('Cache miss! Creating a cache for the item.');
+    console.log('Cache miss! Creating a cache for the item...');
     cache[key] = {}; //initializing cache at whatever the key is.
     cache[key].timestamp = Date.now(); //setting the timestamp value of the object in cache
-    cache[key].data = axios.get(url)// setting the data at the cache object.
-      .then(response => parseWeather(response.body));
+    let weatherData = await axios.get(url);// setting the data at the cache object
+    cache[key].data = parseWeather(weatherData.data.data);
   }
-  console.log(cache[key].data);
   return cache[key].data;
-}
+};
 
 //this function is called if the cache is not present when searched (in the .then part of the above function. It returns an array of Weather objects with the appropriate data.);
 function parseWeather(weatherData) {
   try {
-    const weatherSummaries = weatherData.data.map(day => {
+    const weatherSummaries = weatherData.map(day => {
       return new Weather(day);
     });
     return Promise.resolve(weatherSummaries);
@@ -45,12 +48,5 @@ function parseWeather(weatherData) {
   }
 }
 
-class Weather {
-  constructor(day) {
-    this.date = day.valid_date;
-    this.description =`low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
-    this.key = day.valid_date;
-  }
-}
-
-//constructing a class for the Weather object that we will use.
+//exporting the getWeather folder for use in other areas.
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -8,27 +8,28 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-
 //File routing to each routeHandler
 const root = require('./routeHandlers/root');
-const getWeather = require('./routeHandlers/getWeather');
-const getMovies = require('./routeHandlers/getMovies');
+const weather = require('./routeHandlers/weather.js');
+const getMovies = require('./routeHandlers/getMovies.js');
 
 //referencing our root file to perform all of the code actions.
 app.get('/', root);
 
 app.get('/weather', weatherHandler);
 
-function weatherHandler(req, res) {
-  const { lat, lon } = req.query;
-  getWeather(lat, lon)
-    .then(summaries => res.send(summaries))
-    .catch((error) => {
-      console.error(error);
-      res.status(200).send('Sorry. Something went wrong!');
-    });
+function weatherHandler(request, response) {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  try {
+    console.log(weather(lat, lon));
+    response.send(weather(lat, lon));
+  }
+  catch(err){
+    console.error(err);
+    response.status(200).send('Sorry, something went wrong!');
+  }
 }
-
 //Getting the movie data. References
 app.get('/movie', getMovies);
 
